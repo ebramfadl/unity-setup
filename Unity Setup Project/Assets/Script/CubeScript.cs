16961539887007ebeaa079b9406e76d8f252103c
Ebram Fadl <ebram@ntsal.com>
2024-11-25 19:00:39 +0200

    Ebram change

--- Unity Setup Project/Assets/Script/CubeScript.cs
+++ Unity Setup Project/Assets/Script/CubeScript.cs
@@ -7,7 +7,7 @@ public class CubeScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //hello
+        //ebram
         //Changed from local
         //Changed from github
     }
