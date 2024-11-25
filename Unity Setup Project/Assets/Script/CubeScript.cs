b9e2c2ab48dd1473b1d298c61da7ae5eaa66228c
Ebram Fadl <99660374+ebramfadl@users.noreply.github.com>
2024-11-25 10:49:58 +0200

    Update Script

--- Unity Setup Project/Assets/Script/CubeScript.cs
+++ Unity Setup Project/Assets/Script/CubeScript.cs
@@ -7,7 +7,7 @@ public class CubeScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //hello
     }
 
     // Update is called once per frame
